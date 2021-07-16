c0139829f911d89380faf3421556d4cfb998ce41
zhaochaochao <zhaochaochao@corp.netease.com>
2021-07-16 18:41:25 +0800

    Complete

--- common_utils.hpp
+++ common_utils.hpp
@@ -6,12 +6,41 @@
 #define TRT_TEST__COMMON_UTILS_HPP_
 
 #include <memory>
+#include <ctime>
+#include <cstring>
+#include <sys/time.h>
+#include <cmath>
+#include <random>
+
 #include "NvInfer.h"
 
 class MyLogger : public nvinfer1::ILogger {
   void log(Severity severity, const char* msg) {
-    if (severity <= nvinfer1::ILogger::Severity::kWARNING) {
-      std::cout << msg << std::endl;
+    if (severity <= nvinfer1::ILogger::Severity::kINFO) {
+      /*Generate time str such as 2017-08-05 09:22:55.726*/
+      timeval tv{};
+      gettimeofday(&tv, nullptr);
+      struct tm* tm_now = localtime(&tv.tv_sec);
+      char time_str[30];
+      memset(time_str, 0, sizeof(time_str));
+      strftime(time_str, sizeof(time_str), "%Y-%m-%d %H:%M:%S", tm_now);
+      snprintf(time_str + 19, 5, ".%lu", (unsigned long) round(tv.tv_usec / 1000.0));
+
+      std::string severity_flag;
+      switch (severity) {
+        case nvinfer1::ILogger::Severity::kVERBOSE:severity_flag = "[V]";
+          break;
+        case nvinfer1::ILogger::Severity::kINFO:severity_flag = "[I]";
+          break;
+        case nvinfer1::ILogger::Severity::kWARNING:severity_flag = "[W]";
+          break;
+        case nvinfer1::ILogger::Severity::kERROR:severity_flag = "[E]";
+          break;
+        case nvinfer1::ILogger::Severity::kINTERNAL_ERROR:severity_flag = "[IE]";
+          break;
+      }
+
+      std::cout << "[" << time_str << "] " << severity_flag << " " << msg << std::endl;
     }
   }
 };
@@ -22,5 +51,54 @@ struct TrtDestroyer {
 };
 
 template<typename T> using TrtUniquePtr = std::unique_ptr<T, TrtDestroyer<T> >;
+
+template<typename T>
+inline T RoundUp(T m, T n) { return ((m + n - 1) / n) * n; }
+
+/**
+ * Calc volume of tensor.
+ * @param d: Dimensions of tensor.
+ * @return The volume of tensor.
+ */
+inline int Volume(const nvinfer1::Dims& d) {
+  return std::accumulate(d.d, d.d + d.nbDims, 1, std::multiplies<int>()); // 所有维度shape乘积
+}
+
+/**
+ * Calc volume of tensor.
+ * @param dims: Dimensions of tensor.
+ * @param vec_dim:
+ * @param comps
+ * @param batch: Batch size.
+ * @return The volume of tensor.
+ */
+inline int Volume(nvinfer1::Dims dims, int vec_dim, int comps, int batch = 1) {
+  if (vec_dim != -1) {
+    dims.d[vec_dim] = RoundUp(dims.d[vec_dim], comps);
+  }
+  return Volume(dims) * std::max(batch, 1);
+}
+
+/**
+ * Fill buffer with uniform random.
+ * @param buffer: Buffer.
+ * @param volume: Buffer volume.
+ * @param min: Min val of uniform distribution.
+ * @param max: Max val of uniform distribution.
+ */
+template<typename T>
+inline void FillBuffer(void* buffer, int volume, T min, T max) {
+  T* typed_buffer = static_cast<T*>(buffer);
+  std::default_random_engine engine;
+  if (std::is_integral<T>::value) {
+    std::uniform_int_distribution<int> distribution(min, max);
+    auto generator = [&engine, &distribution]() { return static_cast<T>(distribution(engine)); };
+    std::generate(typed_buffer, typed_buffer + volume, generator);
+  } else {
+    std::uniform_real_distribution<float> distribution(min, max);
+    auto generator = [&engine, &distribution]() { return static_cast<T>(distribution(engine)); };
+    std::generate(typed_buffer, typed_buffer + volume, generator);
+  }
+}
 
 #endif //TRT_TEST__COMMON_UTILS_HPP_
--- main.cpp
+++ main.cpp
@@ -1,22 +1,34 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <sstream>
+#include <algorithm>
 
 #include "cuda_runtime_api.h"
 #include "common_utils.hpp"
+#include "cuda_device.hpp"
 
-MyLogger g_logger;
+static MyLogger g_logger; // Global logger.
+static const int kDefaultDimension = 350; // Batch size of dimension.
+// The binding buffers(<MirroredBuffer, IfInput>).
+static std::vector<std::pair<MirroredBuffer*, bool>> g_binding_buffers;
 
-nvinfer1::ICudaEngine* loadEngine(const std::string& engine, int DLACore=-1) {
+/**
+ * Load trt engine from serialized engine file.
+ * @param engine: Serialized engine file.
+ * @param DLACore: DLA(Deep Learning Accelerator) Core.
+ * @return Trt engine.
+ */
+nvinfer1::ICudaEngine* LoadEngine(const std::string& engine, int DLACore = -1) {
   std::ifstream engine_file(engine, std::ios::binary);
   if (!engine_file) {
     std::cout << "Error opening engine file: " << engine << std::endl;
     return nullptr;
   }
 
-  engine_file.seekg(0, engine_file.end);
-  long int fsize = engine_file.tellg();
-  engine_file.seekg(0, engine_file.beg);
+  engine_file.seekg(0, std::ifstream::end);
+  size_t fsize = static_cast<size_t>(engine_file.tellg());
+  engine_file.seekg(0, std::ifstream::beg);
 
   std::vector<char> engine_data(fsize);
   engine_file.read(engine_data.data(), fsize);
@@ -34,9 +46,121 @@ nvinfer1::ICudaEngine* loadEngine(const std::string& engine, int DLACore=-1) {
   return runtime->deserializeCudaEngine(engine_data.data(), fsize, nullptr);
 }
 
+/**
+ * Setup interface. Check engine bindings. Set default dimension if shape is dynamic.
+ * @param engine: Trt engine.
+ * @param context: Trt execution context.
+ */
+void SetupInterface(const nvinfer1::ICudaEngine* engine, nvinfer1::IExecutionContext* context) {
+  std::stringstream ss;
+  int binding_size = engine->getNbBindings();
+  std::cout << "Model bindings(Input and Output) size: " << binding_size << std::endl;
+
+  for (int b = 0; b < binding_size; b++) {
+    auto dims = context->getBindingDimensions(b);
+
+    const bool dynamic_shape = std::any_of(dims.d, dims.d + dims.nbDims, [](int dim) { return dim == -1; })
+        || engine->isShapeBinding(b);
+
+    // The shape of binding is dynamic. Set default dimension size.
+    if (dynamic_shape) {
+      nvinfer1::Dims static_dims{};
+      static_dims.nbDims = dims.nbDims;
+      std::transform(dims.d,
+                     dims.d + dims.nbDims,
+                     static_dims.d,
+                     [&](int dim) { return dim > 0 ? dim : kDefaultDimension; });
+      context->setBindingDimensions(b, static_dims);
+    }
+
+    dims = context->getBindingDimensions(b);
+    const auto vec_dim = engine->getBindingVectorizedDim(b);
+    const auto comps = engine->getBindingComponentsPerElement(b);
+    const auto data_type = engine->getBindingDataType(b);
+    const auto name = engine->getBindingName(b);
+    const auto is_input = engine->bindingIsInput(b);
+
+    auto* p_mirrored_buffer = new MirroredBuffer();
+    p_mirrored_buffer->allocate(static_cast<size_t>(Volume(dims, vec_dim, comps)));
+    g_binding_buffers.emplace_back(std::make_pair(p_mirrored_buffer, is_input));
+
+    ss.str("");
+    for (int d = 0; d < dims.nbDims; d++) {
+      ss << dims.d[d];
+      if (d < dims.nbDims - 1) {
+        ss << "x";
+      }
+    }
+
+    std::cout << "Binding name: " << name << ", data_type: " << (int) data_type << ", is_input: " << is_input
+              << ", vec_dim: " << vec_dim << ", comps: " << comps << std::endl;
+  }
+}
+
+/**
+ * Fill random input(uniform distribution) into input buffer.
+ */
+void FillRandomInput() {
+  for (auto& buf : g_binding_buffers) {
+    if (buf.second) { // If is input binding buffer.
+      FillBuffer(buf.first->getHostBuffer(), buf.first->getSize(), -1, 1);
+    }
+  }
+}
+
+/**
+ * Run infer.
+ * @param context: Trt execution context.
+ */
+void RunInfer(nvinfer1::IExecutionContext* context) {
+  TrtCudaStream cuda_stream;
+  for (auto& buf : g_binding_buffers) {
+    if (buf.second) { // If is input binding buffer.
+      buf.first->hostToDevice(cuda_stream); // Copy input data from host to gpu device.
+    }
+  }
+
+  void* dev_ptr[g_binding_buffers.size()]; // Prepare device buffer.
+  for (int i = 0; i < g_binding_buffers.size(); i++) {
+    dev_ptr[i] = g_binding_buffers[i].first->getDeviceBuffer();
+  }
+
+  context->enqueueV2(dev_ptr, cuda_stream.get(), nullptr); // Engine infer.
+
+  for (auto& buf : g_binding_buffers) {
+    if (!buf.second) { // If is output binding buffer.
+      buf.first->deviceToHost(cuda_stream); // Copy output data from gpu device to host.
+    }
+  }
+
+  cudaStreamSynchronize(cuda_stream.get()); // Wait stream complete.
+
+  for (auto& buf : g_binding_buffers) {
+    if (!buf.second) { // Show output.
+      auto* ptr = (float*) buf.first->getHostBuffer();
+      std::cout << "Output:" << std::endl;
+      for (int i = 0; i < buf.first->getSize(); i++) {
+        std::cout << ptr[i];
+      }
+    }
+  }
+}
+
 int main(int argc, char** argv) {
   cudaSetDevice(0);
-  loadEngine(argv[1]);
+  nvinfer1::ICudaEngine* engine = LoadEngine(argv[1]);
+  if (engine) {
+    std::cout << "engine load successfully!" << std::endl;
+  } else {
+    std::cout << "engine load failed!" << std::endl;
+    return -1;
+  }
 
+  nvinfer1::IExecutionContext* context = engine->createExecutionContext();
+  SetupInterface(engine, context);
+  FillRandomInput();
+  RunInfer(context);
+
+  cudaDeviceReset();
   return 0;
 }
